c8e021d3a5629f121f33ba7f7a077985ab4b4299
Kim Taehwan <31404686+ktcar214@users.noreply.github.com>
2021-08-29 16:22:40 +0900

    Update prebuild.sh

--- prebuild.sh
+++ prebuild.sh
@@ -4,3 +4,4 @@ gpg --keyserver keyserver.ubuntu.com --recv-keys 647F28654894E3BD457199BE38DBBDC
 gpg --keyserver keyserver.ubuntu.com --recv-keys ABAF11C65A2970B130ABE3C479BE3E4300411886
 gpg --keyserver keyserver.ubuntu.com --recv-keys C5ADB4F3FEBBCE27A3E54D7D9AE4078033F8024D
 gpg --keyserver keyserver.ubuntu.com --recv-keys A2FF3A36AAA56654109064AB19802F8B0D70FC30
+curl -s https://keybase.io/heftig/pgp_keys.asc/?fingerprint\=a2ff3a36aaa56654109064ab19802f8b0d70fc30 | gpg --import 
