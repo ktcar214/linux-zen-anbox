40b123438524a300694ea888310aad54a577aa3a
Kim Taehwan <31404686+ktcar214@users.noreply.github.com>
2021-08-29 15:28:06 +0900

    Create prebuild.sh

---
+++ prebuild.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+gpg --keyserver keyserver.ubuntu.com --recv-keys 647F28654894E3BD457199BE38DBBDC86092693E
+gpg --keyserver keyserver.ubuntu.com --recv-keys ABAF11C65A2970B130ABE3C479BE3E4300411886
+gpg --keyserver keyserver.ubuntu.com --recv-keys C5ADB4F3FEBBCE27A3E54D7D9AE4078033F8024D
+gpg --keyserver keyserver.ubuntu.com --recv-keys A2FF3A36AAA56654109064AB19802F8B0D70FC30
